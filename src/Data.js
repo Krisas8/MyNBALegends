3326ec8c6b6d7b34b7e335a797666957f0baf692
Krisas8 <chris_schulze1985@web.de>
2022-05-04 13:06:39 +0300

    first commit

---
+++ src/Data.js
@@ -0,0 +1,112 @@
+export default [
+    {
+      id: 1,
+      name: 'michael jordan',
+      position: 'shooting guard',
+      image:
+        'https://hoopshype.com/wp-content/uploads/sites/92/2021/11/i_97_bc_1d_michael-jordan.png?w=1000&h=600&crop=1',
+      text:
+        "Michael Jordan, also known by his initials MJ, is a former professional basketball player. He played fifteen seasons in the National Basketball Association (NBA), winning six NBA championships with the Chicago Bulls. Jordan is the principal owner and chairman of the Charlotte Hornets of the NBA. His biography on the official NBA website states: 'By acclamation, Michael Jordan is the greatest basketball player of all time.' He was integral in popularizing the NBA around the world in the 1980s and 1990s, becoming a global cultural icon.",
+    },
+    {
+      id: 2,
+      name: 'dirk nowitzki',
+      position: 'power forward',
+      image:
+        'https://thumbs.dreamstime.com/b/dirk-nowitzki-von-dallas-44151281.jpg',
+      text:
+        'Dirk Nowitzki is a German former professional basketball player who is a special advisor for the Dallas Mavericks of the National Basketball Association (NBA). Listed at 7 ft 0 in (2.13 m), he is widely regarded as one of the greatest power forwards of all time and is considered by many to be the greatest European player of all time. In 2021, he was selected to the NBA 75th Anniversary Team. An alumnus of the DJK Würzburg basketball club, Nowitzki was chosen as the ninth pick in the 1998 NBA draft by the Milwaukee Bucks and was immediately traded to the Dallas Mavericks, where he played his entire 21-year National Basketball Association (NBA) career.',
+    },
+    {
+      id: 3,
+      name: 'lebron james',
+      position: 'small forward',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh4yaQ43Ur4vIsYK6-s7PkUb-Y2ahxSWob1g&usqp=CAU',
+      text:
+        'LeBron James is an American professional basketball player for the Los Angeles Lakers of the National Basketball Association (NBA). Nicknamed "King James", he is widely considered one of the greatest players of all time and is often compared to Michael Jordan in debates over the greatest basketball player ever. James has won four NBA championships, four NBA MVP awards, four NBA Finals MVP awards, three All-Star MVP awards, and two Olympic gold medals. James has scored the most points in the playoffs, the second most career points, and has the seventh most career assists. He has been selected an NBA All-Star 18 times, to the All-NBA Team a record 17 times, and to the NBA All-Defensive First Team five times. He has competed in ten NBA Finals, the third most all time, including eight consecutively between 2011 and 2018. In 2021, James was selected to the NBA 75th Anniversary Team.',
+    },
+    {
+      id: 4,
+      name: "Shaquille O'Neal",
+      position: 'center',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCy0yp2RKnN51pBRLArn2G9N0YwcqgqfGvRg&usqp=CAU',
+      text:
+        " Shaquille O'Neal, known commonly as 'Shaq', is an American former professional basketball player who is a sports analyst on the television program Inside the NBA. O'Neal is regarded as one of the greatest basketball players and centers of all time. He is a 7-foot-1-inch (2.16 m) and 325-pound (147 kg) center who played for six teams over his 19-year career in the National Basketball Association (NBA) and is a four-time NBA champion. After playing college basketball for the LSU Tigers, O'Neal was drafted by the Orlando Magic with the first overall pick in the 1992 NBA draft. He quickly became one of the best centers in the league, winning Rookie of the Year in 1992/93 and leading his team to the 1995 NBA Finals. After four years with the Magic, O'Neal signed as a free agent with the Los Angeles Lakers.",
+    },
+    {
+      id: 5,
+      name: "Gary Payton",
+      position: 'point guard',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpXcfev2-QZHSvMkul7Mtt-BKUNrRs-qT44w&usqp=CAU',
+      text:
+        "Gary Payton is an American former professional basketball player who played the point guard position. Widely considered one of the greatest point guards of all time, he is best known for his 13-year tenure with the Seattle SuperSonics, and holds Seattle franchise records in points, assists, and steals. He also played with the Milwaukee Bucks, Los Angeles Lakers, Boston Celtics, and Miami Heat. Payton won an NBA championship with the Heat in 2006. Nicknamed 'The Glove' for his defensive abilities, Payton was inducted into the Naismith Basketball Hall of Fame in 2013. In October 2021, Payton was honored as one of the league's greatest players of all-time by being named to the NBA 75th Anniversary Team. Payton is the first point guard to win the NBA Defensive Player of the Year award, and the only point guard winner in the award's first 39 years of existence, until Marcus Smart was selected in 2022.",
+    },
+    {
+      id: 6,
+      name: "ray allen",
+      position: 'shooting guard',
+      image:
+        'https://d1l5jyrrh5eluf.cloudfront.net/wp-content/uploads/2020/11/GettyImages-74334828.jpg',
+      text:
+        "Ray Allen is an American former professional basketball player. He played 18 seasons in the National Basketball Association (NBA) and was inducted into the Naismith Memorial Basketball Hall of Fame as a player in September 2018. Allen is widely considered to be one of the greatest three-point shooters of all-time, and he held the record for most three-pointers made in a career before the record was broken by Stephen Curry in 2021. Allen played college basketball for the Connecticut Huskies for three seasons, gaining a reputation as an efficient long-range shooter. He entered the NBA in 1996 as the fifth overall selection. In the NBA, he developed into a prolific scorer for the Milwaukee Bucks, featuring alongside Glenn Robinson and Sam Cassell as the team achieved playoff success. However, the trio were unable to capture a championship, and Allen was traded to the Seattle SuperSonics. In Seattle, Allen's reputation as a scorer was solidified; he would break several league records for three-point and free throw shooting. Despite this, a title still eluded Allen, and he was traded to the Boston Celtics in 2007.",
+    },
+    {
+      id: 7,
+      name: "reggie miller",
+      position: 'shooting guard',
+      image:
+        'https://fadeawayworld.net/.image/t_share/MTgwMTMyNjk3NjYxNDQ5NTYw/reggie-miller-would-average-30-just-on-threes-alone-says-former-teammate.jpg',
+      text:
+        "Reggie Miller is an American former professional basketball player who played his entire 18-year National Basketball Association (NBA) career with the Indiana Pacers. Miller was known for his precision three-point shooting, especially in pressure situations and most notably against the New York Knicks, for which he earned the nickname 'Knick Killer.' When he retired, he held the record for most career 3-point field goals made. He is currently fourth on the list behind Stephen Curry, Ray Allen, and James Harden. A five-time All-Star selection, Miller led the league in free throw percentage five times and won a gold medal in the 1996 Summer Olympics. Miller is widely regarded as the Pacers' greatest player of all time. His No. 31 was retired by the team in 2006. Currently, he works as an NBA commentator for TNT and college basketball analyst for CBS Sports. Miller was inducted into the Naismith Memorial Basketball Hall of Fame in 2012 and named to the NBA 75th Anniversary Team in 2021.",
+    },
+    {
+      id: 8,
+      name: "vince carter",
+      position: 'shooting guard',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoBeSIddhg_nGJC2C0ytZinu0PT2Wp3lqMuQ&usqp=CAU',
+      text:
+        "Vincent Carter is an American former professional basketball player and current basketball analyst for ESPN. He primarily played the shooting guard and small forward positions, but occasionally played power forward later in his NBA career. He was an eight-time All-Star and a two-time All-NBA Team selection. He is the only player in NBA history to play 22 seasons and in four different decades, from his debut in 1999 to his retirement in 2020. He entertained crowds with his leaping ability and slam dunks, earning him nicknames such as 'Vinsanity', 'Air Canada', and 'Half Man, Half Amazing'. He has been ranked as the greatest dunker of all time by numerous players, journalists, and by the National Basketball Association (NBA). In addition to his dunking prowess, he was a prolific three-point shooter, making the sixth most three-point field goals in league history.",
+    },
+    {
+      id: 9,
+      name: "tim duncan",
+      position: 'power forward',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8teGEov_ngW5--SrkaHlMQGpAV2TXeT-A9Q&usqp=CAU',
+      text:
+        "Tim Duncan is an American former professional basketball player and coach. Nicknamed 'the Big Fundamental', he is widely regarded as the greatest power forward of all time and one of the greatest players in NBA history. He spent his entire 19-year playing career with the San Antonio Spurs. He was inducted into the Naismith Memorial Basketball Hall of Fame in 2020 and named to the NBA 75th Anniversary Team in 2021. Duncan started out as an aspiring swimmer and only began playing basketball in ninth grade, when Hurricane Hugo destroyed the only available Olympic-sized pool in his homeland of Saint Croix, U.S. Virgin Islands. In high school, he played basketball for St. Dunstan's Episcopal. In college, Duncan played for the Wake Forest Demon Deacons, and in his senior year, he received the John Wooden Award and was named the Naismith College Player of the Year and the USBWA College Player of the Year. After graduating from college, Duncan was the NBA Rookie of the Year after being selected by San Antonio with the first overall pick in the 1997 NBA draft. He primarily played the power forward position and also played center throughout his career. He is a five-time NBA champion, a two-time NBA MVP, a three-time NBA Finals MVP, a 15-time NBA All-Star, and the only player to be selected to both the All-NBA and All-Defensive Teams for 13 consecutive seasons.",
+    },
+    {
+      id: 10,
+      name: "scottie pippen",
+      position: 'small forward',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRamC-Znrd8EzWE82ZDckNnF3FySZWHh-sUVQ&usqp=CAU',
+      text:
+        "Scotty Pippen, usually spelled Scottie Pippen, is an American former professional basketball player. He played 17 seasons in the National Basketball Association (NBA), winning six NBA championships with the Chicago Bulls. Considered one of the greatest small forwards of all time, Pippen, along with Michael Jordan, played an important role in transforming the Bulls into a championship team and in popularizing the NBA around the world during the 1990s. Pippen was named to the NBA All-Defensive First Team eight consecutive times and the All-NBA First Team three times. He was a seven-time NBA All-Star and was the NBA All-Star Game MVP in 1994. He was named one of the 50 Greatest Players in NBA History during the 1996/97 season, and is one of four players to have his jersey retired by the Chicago Bulls (the others being Jerry Sloan, Bob Love, and Jordan). His biography on the Naismith Basketball Hall of Fame's website states that 'the multidimensional Pippen ran the court like a point guard, attacked the boards like a power forward, and swished the nets like a shooting guard'. During his 17-year career, he played 12 seasons with the Bulls, one with the Houston Rockets and four with the Portland Trail Blazers, making the postseason 16 straight times. In October 2021, Pippen was again honored as one of the leagues greatest players of all-time by being named to the NBA 75th Anniversary Team.",
+    },
+    {
+      id: 11,
+      name: "jason kidd",
+      position: 'point guard',
+      image:
+        'https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Fvalleyofthesuns.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2017%2F07%2F72354451.jpeg',
+      text:
+        "Jason Kidd is an American professional basketball coach and former player who is the head coach for the Dallas Mavericks of the National Basketball Association (NBA). Playing point guard in the NBA, Kidd was a 10-time NBA All-Star, a five-time All-NBA First Team member, and a nine-time NBA All-Defensive Team member. He won an NBA championship in 2011 as a member of the Dallas Mavericks and was a two-time gold medal winner in the Olympics with the U.S. national team in 2000 and 2008. He was inducted as a player into the Naismith Memorial Basketball Hall of Fame. In October 2021, Kidd was honored as one of the league's greatest players of all time by being named to the NBA 75th Anniversary Team. Kidd played college basketball for the California Golden Bears and was drafted second overall by the Dallas Mavericks in the first round of the 1994 NBA draft. He was named co NBA Rookie of the Year in his first season with the Mavericks, along with Grant Hill. Then, from 1996 to 2001, Kidd played for the Phoenix Suns and later for the New Jersey Nets from 2001 to 2008. He led the Nets to two consecutive NBA Finals appearances in 2002 and 2003. In the middle of the 2007/08 season, Kidd was traded back to Dallas. At age 38, Kidd won his only NBA championship when Dallas defeated Miami in the 2011 finals. He finished his playing career in 2013 with the New York Knicks. The following season, he became the head coach of the Nets, who had relocated from New Jersey to Brooklyn. After one season, he was traded to the Milwaukee Bucks, where he coached for four seasons until he was fired mid-season in 2018.",
+    },
+    {
+      id: 12,
+      name: "allen iverson",
+      position: 'point guard',
+      image:
+        'https://thumbs.dreamstime.com/b/allen-iverson-21775342.jpg',
+      text:
+        "Allen Iverson is an American former professional basketball player. Nicknamed 'the Answer' and 'AI', he played 14 seasons in the National Basketball Association (NBA) at both the shooting guard and point guard positions. Iverson won NBA Rookie of the Year Award in 1997 and was an 11-time NBA All-Star, won the All-Star game MVP award in 2001 and 2005, and was the NBA's Most Valuable Player (MVP) in 2001. He was inducted into the Naismith Memorial Basketball Hall of Fame in 2016. In October 2021, he was named to the NBA 75th Anniversary Team. Iverson is often regarded as one of the greatest scorers and one of the most influential players in NBA history. Iverson attended Bethel High School in Hampton, Virginia, and was a dual-sport athlete. After high school, Iverson played college basketball with the Georgetown Hoyas for two years, where he set the school record for career scoring average (23.0 points per game) and won Big East Defensive Player of the Year awards both years. His regular season career scoring average of 26.7 points per game ranks seventh all-time, and his playoff career scoring average of 29.7 points per game is second only to Michael Jordan. Iverson represented the United States at the 2004 Summer Olympics, winning the bronze medal.",
+    },
+  ];
+  
+ 
